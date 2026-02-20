d102bcda98c041449fac41bcffb134e67fa44c63
Jay Barbhaiya <jay.barbhaiya@sap.com>
2026-02-20 06:02:07 +0100

    theme and copilot working setup

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,4 +2,16 @@ return {
   "github/copilot.vim",
   cmd = "Copilot",
   opts = {},
+  config = function()
+    -- Disable default Tab mapping to avoid conflicts
+    vim.g.copilot_no_tab_map = true
+
+    -- Enable Copilot automatically on startup
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function() vim.cmd "Copilot enable" end,
+    })
+
+    -- Optional: Map <C-J> to accept Copilot suggestions in insert mode
+    vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { expr = true, silent = true, noremap = true })
+  end,
 }
