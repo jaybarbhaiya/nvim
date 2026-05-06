03bf044612d3a4c2498605cb9defd5c98d44581c
Jay Barbhaiya <jay.barbhaiya@sap.com>
2026-05-07 01:43:01 +0200

    auto dark mode and update colorscheme

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,9 @@
+return {
+  "rebelot/kanagawa.nvim",
+  name = "kanagawa",
+  priority = 1000,
+  config = function ()
+    -- vim.o.background = "light"
+    vim.cmd("colorscheme kanagawa-dragon")
+  end
+}
--- lua/plugins/papercolor_theme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "NLKNguyen/papercolor-theme",
-  name = "papercolor",
-  priority = 1000,
-  config = function ()
-    vim.o.background = "light"
-    vim.cmd("colorscheme PaperColor")
-  end
-}
