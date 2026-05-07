61bb973954a17332a1c3a2c02955dc958e83e88b
Jay Barbhaiya <jay.barbhaiya@sap.com>
2026-05-07 22:56:54 +0200

    remove kanagawa theme and use the default astrovim theme

--- lua/plugins/kanagawa.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "rebelot/kanagawa.nvim",
-  name = "kanagawa",
-  priority = 1000,
-  config = function ()
-    -- vim.o.background = "light"
-    vim.cmd("colorscheme kanagawa-dragon")
-  end
-}
